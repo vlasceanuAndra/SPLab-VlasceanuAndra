d267406c0a2c9da75046ef3506dfbf557f7dbcd2
andravlasceanu <andravlasceanu6@gmail.com>
2025-10-23 00:51:07 +0300

    Initial commit

---
+++ src/main/java/org/example/splaborator/SpLaboratorApplication.java
@@ -0,0 +1,13 @@
+package org.example.splaborator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpLaboratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpLaboratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/splaborator/SpLaboratorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.splaborator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpLaboratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
