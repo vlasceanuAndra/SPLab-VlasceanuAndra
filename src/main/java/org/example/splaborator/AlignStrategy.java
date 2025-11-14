65d660ca341b41c1492f3cb824365a17b45d2230
andravlasceanu <andravlasceanu6@gmail.com>
2025-11-14 19:08:13 +0200

    Initial commit

---
+++ src/main/java/org/example/splaborator/AlignStrategy.java
@@ -0,0 +1,5 @@
+package org.example.splaborator;
+
+public interface AlignStrategy {
+    void render(String text);
+}
--- src/main/java/org/example/splaborator/SpLaboratorApplication.java
+++ src/main/java/org/example/splaborator/SpLaboratorApplication.java
@@ -8,6 +8,7 @@ public class SpLaboratorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpLaboratorApplication.class, args);
+        System.out.println("Print fara align:");
         Book abc = new Book("abc");
         Author LReb = new Author("Liviu Rebreanu");
         abc.addAuthor(LReb);
@@ -15,16 +16,29 @@ public class SpLaboratorApplication {
         Section cap2 = new Section("Capitol 2");
         Section cap3 = new Section("Capitol 3");
         Section cap4 = new Section("Subcapitolul 1.1");
-        abc.addContent(new Paragraph("Cuvant inainte: Multumesc celor..."));
+        Paragraph p1 = new Paragraph("Cuvant inainte: Multumesc celor...");
+        abc.addContent(p1);
         abc.addContent(cap1);
-        cap1.add(new Paragraph("Primul capitol incepe"));
+        Paragraph p2 = new Paragraph("Primul capitol incepe");
+        cap1.add(p2);
         cap1.add(cap4);
-        cap4.add(new Paragraph("Imi place sa scriu"));
+        Paragraph p3 = new Paragraph("Imi place sa scriu");
+        cap4.add(p3);
         abc.addContent(cap2);
-        cap2.add(new Paragraph("Al doilea capitol capitol incepe"));
+        Paragraph p4 = new Paragraph("Al doilea capitol capitol incepe");
+        cap2.add(p4);
         cap2.add(cap3);
         cap3.add(new Image("Imagine capitol 3"));
         abc.print();
+
+        System.out.println("Print cu align:");
+        System.out.println(" ");
+        p1.setAlignStrategy(new AlignLeft());
+        p2.setAlignStrategy(new AlignRight());
+        p3.setAlignStrategy(new AlignCenter());
+        p4.setAlignStrategy(new AlignRight());
+        abc.print();
+
     }
 
 }
